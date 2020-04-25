b2f6215bf359fd88a554959927faed746e6be7c9
angelica.jue <angelica_jue@yahoo.com>
2020-04-25 15:42:55 -0700

    added number correct and sketched out queries for sql

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 const port = 8080; 
 const server = http.createServer(app); 
 
+// Helpers
 const colors = {
   red: '#b01320',
   hotPink: '#c325db',
@@ -35,21 +36,126 @@ const getWord = function() {
   return word;
 };
 
+// Database storage in API
+const numberCorrect = {
+  correct: 0,
+  skip: 0,
+  wrong: 0,
+}
+
+// Postgres
+const config = require('./postgres/config');
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  user: config.pgUser,
+  host: config.pgHost,
+  database: config.pgDatabase,
+  password: config.pgPassword,
+  port: config.pgPort
+});
+pgClient.on('error', () => console.log('Lost Postgres connection'));
+
+pgClient
+  .query(
+    `
+    CREATE TABLE IF NOT EXISTS players (
+      id TEXT NOT NULL,
+      playerName TEXT NOT NUll,
+      color TEXT NOT NULL,
+      active BOOLEAN DEFAULT false,
+      PRIMARY KEY (id)
+    )
+    `
+  )
+  .catch(err => console.log(err));
+
+pgClient
+  .query(
+    `
+    CREATE TABLE IF NOT EXISTS clues (
+      playerName TEXT NOT NUll,
+      clue TEXT,
+      PRIMARY KEY (playerName)
+    )
+    `
+  )
+  .catch(err => console.log(err));
+
+// Sockets
 const io = require('socket.io')(server);
 
 io.on('connection', socket => {
   console.log('New client connected', socket.id);
   
   socket.on('chooseColor', data => {
-    console.log('chose color', data.color);
     socket.broadcast.emit('removeColors', { color: data.color });
+  });
+
+  socket.on('submitSetUp', async data => {
+    const { name, color } = data;
+    await pgClient
+      .query(
+        `INSERT INTO players (id, playerName, color, active) VALUES ($1, $2, $3, $4)`, [socket.id, name, color, false]
+      )
+      .catch(e => console.log(`Creating player error: ${e}`));
   });
 
   socket.on('startGame', () => {
     console.log('starting game with active player');
     let activeWord = getWord();
+    // choose activePlayer and rotate through
+    // select player and make active true
     io.emit('startingGame', { activePlayer: 'testPlayer', activeColor: 'red', activeWord });
+  });
+
+  socket.on('submitClue', async data => {
+    const { name, clue } = data;
+    await pgClient
+      .query(
+        `INSERT INTO clues (playerName, clue) VALUES ($1, $2)`, [name, clue]
+      )
+      .catch(e => console.log(`Clue input error: ${e}`));
+
+    // if (# clues === # players - 1) {
+    //   // get all clues SELECT clue from clues
+    //   // get all players
+    //   // get all colors
+    //   socket.emit('checkClues', { clues });
+    // }
+  });
+
+  socket.on('removeClue', async data => {
+    const { clue } = data;
+    // const clue = await pgClient
+    //   .query(
+    //     `SELECT clue from clues
+    //     WHERE clue=($1)`, [clue]
+    //     // delete clue
+    //   )
+    //   .catch(e => console.log(`Clue remove error: ${e}`));
+  });
+
+  socket.on('finishCheckingClues', async () => {
+    // const clues = await pgClient
+    //   .query(
+    //     `SELECT clue from clues`
+    //   )
+    //   .then(
+    //     io.emit('sendingClues', { clues })
+    //   )
+    //   .catch(e => console.log(`Clue retrieval error: ${e}`));
+  });
+
+  socket.on('updateCorrect', data => {
+    const { status } = data;
+    const newNumberCorrect = {...numberCorrect};
+    newNumberCorrect[status]++;
+    numberCorrect = newNumberCorrect;
+    
+    io.emit('numberCorrect', { numberCorrect });
   })
+
 /*
   // Create new game room
   socket.on('createGame', data => {    
@@ -123,52 +229,23 @@ io.on('connection', socket => {
   });
 });
 
-
-// Postgres
-const config = require('./postgres/config');
-
-const { Pool } = require('pg');
-const pgClient = new Pool({
-  user: config.pgUser,
-  host: config.pgHost,
-  database: config.pgDatabase,
-  password: config.pgPassword,
-  port: config.pgPort
-});
-pgClient.on('error', () => console.log('Lost Postgres connection'));
-
-pgClient
-  .query(
-    `
-    CREATE TABLE IF NOT EXISTS players (
-      id uuid,
-      playerName TEXT NOT NUll,
-      color TEXT NOT NULL,
-      active BOOLEAN DEFAULT false,
-      PRIMARY KEY (id)
-    )
-    `
-  )
-  .catch(err => console.log(err));
-
-
 // Routes
 
-app.post('/v1/player', async (req, res) => {
-  const { name, color } = req.body;
-  const id = uuidv4();
-  const player = await pgClient
-    .query(
-      `INSERT INTO players (id, playerName, color, active) VALUES ($1, $2, $3, $4)`, [id, name, color, false]
-    )
-    .catch(e => {
-      res
-        .status(500)
-        .send('Encountered an internal error when creating player');
-    });
-    // console.log('created player');
-  return res.status(201).send(`Created ${color} player, ${name}`);
-});
+// app.post('/v1/player', async (req, res) => {
+//   const { name, color } = req.body;
+//   const id = uuidv4();
+//   const player = await pgClient
+//     .query(
+//       `INSERT INTO players (id, playerName, color, active) VALUES ($1, $2, $3, $4)`, [id, name, color, false]
+//     )
+//     .catch(e => {
+//       res
+//         .status(500)
+//         .send('Encountered an internal error when creating player');
+//     });
+//     // console.log('created player');
+//   return res.status(201).send(`Created ${color} player, ${name}`);
+// });
 
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
