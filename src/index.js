a348316cd1ca905d67fc89f0f8ce08cdeb583cfd
angelica.jue <angelica_jue@yahoo.com>
2020-04-26 18:23:29 -0700

    fixed sql queries

--- src/index.js
+++ src/index.js
@@ -43,6 +43,11 @@ const numberCorrect = {
   wrong: 0,
 }
 
+const activePlayers = [];
+
+let numberOfPlayers;
+let roundNumber;
+
 // Postgres
 const config = require('./postgres/config');
 
@@ -75,6 +80,7 @@ pgClient
     `
     CREATE TABLE IF NOT EXISTS clues (
       playerName TEXT NOT NUll,
+      color TEXT,
       clue TEXT,
       PRIMARY KEY (playerName)
     )
@@ -82,10 +88,22 @@ pgClient
   )
   .catch(err => console.log(err));
 
+pgClient
+  .query(
+    `
+    CREATE TABLE IF NOT EXISTS roundstatus (
+      round INT,
+      status text
+    )
+    `
+  )
+  .catch(err => console.log(err));
+
+
 // Sockets
 const io = require('socket.io')(server);
 
-io.on('connection', socket => {
+io.on('connection', socket => {  
   console.log('New client connected', socket.id);
   
   socket.on('chooseColor', data => {
@@ -96,132 +114,91 @@ io.on('connection', socket => {
     const { name, color } = data;
     await pgClient
       .query(
-        `INSERT INTO players (id, playerName, color, active) VALUES ($1, $2, $3, $4)`, [socket.id, name, color, false]
+        `INSERT INTO players (id, playerName, color, active) 
+        VALUES ($1, $2, $3, $4)`, [socket.id, name, color, false]
       )
       .catch(e => console.log(`Creating player error: ${e}`));
   });
 
-  socket.on('startRound', () => {
+  socket.on('startRound', async () => {
     //console.log('starting round with active player');
     let activeWord = getWord();
     // choose activePlayer and rotate through
     // select player and make active true
-    io.emit('startingRound', { activePlayer: 'testPlayer', activeColor: 'red', activeWord });
+    io.emit('startingRound', { activePlayer: 'guesser', activeColor: 'pink', activeWord });
+    
+    const players = await pgClient
+      .query('SELECT playerName FROM players')
+      .catch(e => console.log(`Couldn't get playerName: ${e}`));
+
+    numberOfPlayers = players.rows.length;
+    console.log('numPlayers from start round', numberOfPlayers);
+    roundNumber++;
   });
 
   socket.on('submitClue', async data => {
-    const { name, clue } = data;
+    const { name, color, clue } = data;
     await pgClient
       .query(
-        `INSERT INTO clues (playerName, clue) VALUES ($1, $2)`, [name, clue]
+        `INSERT INTO clues (playerName, color, clue) 
+        VALUES ($1, $2, $3)`, [name, color, clue]
       )
       .catch(e => console.log(`Clue input error: ${e}`));
 
-    // if (# clues === # players - 1) {
-    //   // get all clues SELECT clue from clues
-    //   // get all players
-    //   // get all colors
-    //   socket.emit('checkClues', { clues });
-    // }
+    const clues = await pgClient
+      .query('SELECT * FROM clues')
+      .catch(e => console.log(`Couldn't get clues: ${e}`));
+    
+      console.log('number of clues', clues.rows.length);
+      console.log('number of players', numberOfPlayers);
+
+    if (clues.rows.length === (numberOfPlayers - 1)) {
+      io.emit('checkClues', { clues: clues.rows });
+    }
   });
 
   socket.on('removeClue', async data => {
     const { clue } = data;
-    // const clue = await pgClient
-    //   .query(
-    //     `SELECT clue from clues
-    //     WHERE clue=($1)`, [clue]
-    //     // delete clue
-    //   )
-    //   .catch(e => console.log(`Clue remove error: ${e}`));
+    await pgClient
+      .query(
+        `DELETE FROM clues
+        WHERE clue=$1`, [clue]
+      )
+      .catch(e => console.log(`Clue remove error: ${e}`));
+
+    // io.emit('removingClue');
   });
 
   socket.on('finishCheckingClues', async () => {
-    // const clues = await pgClient
-    //   .query(
-    //     `SELECT clue from clues`
-    //   )
-    //   .then(
-    //     io.emit('sendingClues', { clues })
-    //   )
-    //   .catch(e => console.log(`Clue retrieval error: ${e}`));
+    const clues = await pgClient
+      .query('SELECT * FROM clues')
+      .catch(e => console.log(`Clue retrieval error: ${e}`));
+
+    io.emit('sendingClues', { clues: clues.rows })
   });
 
-  socket.on('updateCorrect', data => {
+  socket.on('updateCorrect', async data => {
     const { status } = data;
-    const newNumberCorrect = {...numberCorrect};
-    newNumberCorrect[status]++;
-    numberCorrect = newNumberCorrect;
+
+    await pgClient
+      .query(
+        `INSERT INTO roundstatus (round, status)
+        VALUES (${roundNumber}, ${status})`
+      )
+      .catch(e => console.log(`Trouble updating correct: ${e}`));
     
+    const rounds = await pgClient
+      .query('SELECT * FROM roundstatus');
+    
+    rounds.rows.forEach(row => {
+      const newNumberCorrect = {...numberCorrect};
+      newNumberCorrect[row.status]++;
+      numberCorrect = newNumberCorrect;
+    });
+
     io.emit('numberCorrect', { numberCorrect });
   });
-/*
-  // Create new game room
-  socket.on('createGame', data => {    
-    ++gameState.numOfRooms;
-    let roomId = 'room-' + gameState.numOfRooms;
-    socket.join(roomId);
-    gameState[roomId] = {
-      board: generateBoard(),
-      players: {
-        [socket.id]: data.role,
-      }
-    };
-    //console.log('gameState', gameState);
-    socket.emit('creatingGame', {
-      roomId,
-      board: gameState[roomId].board,
-    });    
-  });
 
-  //Connect new player to requested room
-  socket.on('joinGame', data => {
-    const { role, roomId } = data;
-    if (_.isUndefined(gameState[roomId]) || _.isUndefined(gameState[roomId].board)) {
-      socket.emit('joiningGame', {
-        error: 'No game in progress with that Room ID!'
-      });
-    } else {
-      socket.join(roomId);
-      gameState[roomId].players[socket.id] = role;
-      socket.emit('joiningGame', {
-        roomId: roomId,
-        board: gameState[roomId].board
-      });
-    }
-  })
-
-  //Handle tile clicked
-  socket.on('clickTile', data => {
-    const { roomId } = data;
-    gameState[roomId].board = data.board;
-    // emits to all except sender
-    socket.to(roomId).emit('updateBoard', {
-      board: gameState[roomId].board
-    });
-  });
-
-  socket.on('getNewBoard', data => {
-    const { roomId } = data;
-    gameState[roomId].board = generateBoard();
-    // emits to everyone including sender
-    io.in(roomId).emit('updateBoard', {
-      board: gameState[roomId].board,
-      role: {}
-    });
-  });
-
-  socket.on('leaveRoom', data => {
-    const { roomId } = data;
-    //console.log('player', socket.id, 'left', roomId);
-    socket.leave(roomId);
-    delete gameState[roomId].players[socket.id];
-    
-    // if (Object.keys(gameState[roomId].players).length === 0) {
-    //   delete gameState[roomId]
-    // }
-  });
-*/
   socket.on('disconnect', () => {
     console.log('Client disconnected', socket.id);
     
